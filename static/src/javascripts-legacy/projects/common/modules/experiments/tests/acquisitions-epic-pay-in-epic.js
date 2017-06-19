c11ef7730cdf3e79ff17c3e0de652756083d5fc5
Sam Desborough <sam.desborough@gmail.com>
2017-06-19 14:32:18 +0100

    Tidy up payment test

--- static/src/javascripts-legacy/projects/common/modules/experiments/tests/acquisitions-epic-pay-in-epic.js
+++ static/src/javascripts-legacy/projects/common/modules/experiments/tests/acquisitions-epic-pay-in-epic.js
@@ -8,7 +8,7 @@ define([
     'common/modules/commercial/acquisitions-copy',
     'common/modules/commercial/contributions-utilities',
     'ophan/ng',
-    'raw-loader!common/views/acquisitions-epic-pay-in-epic.html'
+    'raw-loader!common/views/acquisitions-epic-iframe.html'
 ], function (
     template,
     config,
@@ -19,164 +19,18 @@ define([
     acquisitionsCopy,
     contributionsUtilities,
     ophan,
-    payInEpicTemplate
+    iframeTemplate
 ) {
-
-    // Click event listener added to contribution button with this id.
-    var CONTRIBUTION_BUTTON_ID = 'pay-in-epic-contribution-button';
-
-    // Values required by the token function in the stripe config, that
-    // (1) can only be (easily) inferred at run-time; and
-    // (2) can not be passed to the function directly.
-    var state = {
-        amount: null,
-        campaignCode: null
-    };
-
-    function getReadersCurrency() {
-
-        var region = geolocation.getSupporterPaymentRegion();
-
-        function currency(isoCode, symbol) {
-            return {
-                // Used for Stripe and form submitted to contributions endpoint.
-                // https://www.iban.com/currency-codes.html
-                isoCode: isoCode,
-                // Used client-side
-                symbol: symbol
-            }
-        }
-
-        if (region === 'GB') {
-            return currency('GBP', '£')
-        }
-
-        if (region === 'US') {
-            return currency('USD', '$')
-        }
-
-        if (region === 'AU') {
-            return currency('AUD', 'AUD$')
-        }
-
-        if (region === 'CA') {
-            return currency('CAD', 'CAD$')
-        }
-
-        if (region === 'EU') {
-            return currency('EUR', "€")
-        }
-
-        // International and default currency.
-        return currency('USD', '$')
-    }
-
-    var readersCurrency = getReadersCurrency();
-
-    // The callback used to execute a charge for a reader using a token provided by Stripe.
-    // https://stripe.com/docs/checkout/tutorial#tokens
-    function processStripeContribution(token) {
-
-        function getFormData(token) {
-
-            function getOphanField(field) {
-                // Default value not_found keeps with convention set in contribution utilities.
-                return (config.ophan && config.ophan[field]) || 'not_found'
-            }
-
-            // In https://github.com/guardian/contributions-frontend see:
-            // controllers.forms.ContributionRequest.contributionForm
-            return {
-                // We don't collect the reader's name in-Epic currently.
-                name: 'unknown',
-                currency: readersCurrency.isoCode,
-                amount: state.amount,
-                email: token.email,
-                token: token.id,
-                // Since we don't ask for marketing opt-in, assume the reader doesn't want to be contacted.
-                marketing: false,
-                ophanPageviewId: getOphanField('pageViewId'),
-                ophanBrowserId: getOphanField('browserId'),
-                intcmp: state.campaignCode,
-            }
-        }
-
-        function handleResponse() {
-            // FIXME
-
-        }
-
-        fetch('https://contribute.theguardian.com/stripe/pay', {
-            credentials: 'same-origin',
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(getFormData(token))
-        }).then(handleResponse);
-    }
-
-    function getStripeHandler() {
-        // Script must be loaded. Using a local copy is unsupported.
-        // https://stripe.com/docs/checkout#integration
-        return loadScript.loadScript('https://checkout.stripe.com/checkout.js').then(function() {
-            // See: https://stripe.com/docs/checkout#integration-custom - Configuration Options
-            var conf = {
-                key: 'pk_test_35RZz9AAyqErQshL410RDZMs',
-                name: 'The Guardian',
-                description: 'Make a contribution',
-                allowRememberMe: false,
-                token: processStripeContribution,
-                currency: readersCurrency.isoCode
-            };
-
-            conf
-
-            // StripeCheckout.configure(conf);
-
-        });
-    }
-
-    function addEventListeners(handler) {
-
-        // TODO: implement
-        function getContributionAmount() {
-            return 20;
-        }
-
-        function recordContributionButtonClicked() {
-            ophan.record({
-                component: 'epic_contribution_button',
-                value: 'clicked'
-            })
-        }
-
-        document.getElementById(CONTRIBUTION_BUTTON_ID).addEventListener('click', function(e) {
-            // Recording this allows the success measure of contributions / views to be factored out as:
-            // (contributions / clicks) * (clicks / views), which may prove insightful.
-            // Note: to simplify we are assuming each reader clicks on the button at most once per impression.
-            recordContributionButtonClicked();
-
-            // Store the amount for use by the payment form.
-            state.amount = getContributionAmount();
-
-            // Amount configured here, since it is not known when the Stripe checkout is configured.
-            handler.open({ amount: state.amount });
-            e.preventDefault();
-        });
-
-        // Close Checkout on page navigation:
-        window.addEventListener('popstate', function() {
-            handler.close();
-        });
-    }
+    //var readersCurrency = geolocation.getCurrency(geolocation.getSync());
 
     return contributionsUtilities.makeABTest({
         id: 'AcquisitionsEpicPayInEpic',
         campaignId: 'epic_pay_in_epic',
 
-        start: '2017-06-14',
+        start: '2017-06-19',
         expiry: '2018-07-20',
 
-        author: 'Guy Dawson',
+        author: 'Guy Dawson & Sam Desborough',
         description: 'Test whether letting readers pay in-Epic will lead to a higher conversion rate',
         successMeasure: 'Conversion rate',
         idealOutcome: 'The pay in-Epic variant smashes the control out of the park',
@@ -194,20 +48,19 @@ define([
                 isUnlimited: true,
 
                 template: function(variant) {
-                    return template(payInEpicTemplate, {
-                        copy: acquisitionsCopy.control,
-                        membershipUrl: variant.options.membershipURL,
-                        contributionUrl: variant.options.contributeURL,
-                        contributionButtonId: CONTRIBUTION_BUTTON_ID,
-                        currencySymbol: readersCurrency.symbol
-                    });
+                    return template(iframeTemplate, {
+                        componentName: variant.options.componentName,
+                        id: variant.options.iframeId,
+                        iframeUrl: 'http://localhost:9111/components/epic/inline-payment',
+                    })
                 },
 
-                test: function(render, variant) {
-                    // Store the generated campaign code for use by the payment form.
-                    state.campaignCode = variant.options.campaignCode;
-                    render().then(getStripeHandler).then(addEventListeners);
+                test: function(render) {
+                    loadScript.loadScript('https://www.paypalobjects.com/api/checkout.js')
+                        .then(function() { render(); });
                 },
+
+                usesIframe: true
             }
         ]
     });
