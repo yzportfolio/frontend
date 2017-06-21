c40d4a89112d10db0606bcd5e3ce518fd6d4c399
Alex Sanders <a@snd.rs>
2017-06-21 11:20:36 +0100

    convert static/src/javascripts-legacy/projects/commercial/modules/third-party-tags/audience-science-gateway.js

--- static/src/javascripts-legacy/projects/commercial/modules/third-party-tags/audience-science-gateway.js
+++ static/src/javascripts-legacy/projects/commercial/modules/third-party-tags/audience-science-gateway.js
@@ -1,11 +1,7 @@
-define([
-    'lib/config'
-], function (config) {
-    var audienceScienceGatewayUrl = '//js.revsci.net/gateway/gw.js?csid=F09828&auto=t&bpid=theguardian';
+import config from 'lib/config';
+var audienceScienceGatewayUrl = '//js.revsci.net/gateway/gw.js?csid=F09828&auto=t&bpid=theguardian';
 
-    return {
-        shouldRun: config.page.edition === 'UK' && config.switches.audienceScienceGateway,
-        url: audienceScienceGatewayUrl
-    };
-
-});
+export default {
+    shouldRun: config.page.edition === 'UK' && config.switches.audienceScienceGateway,
+    url: audienceScienceGatewayUrl
+};
