37df7e68e73e3a280c7f720f55cf0dc56c0d8b6a
Alex Sanders <a@snd.rs>
2017-06-21 11:20:41 +0100

    convert static/src/javascripts-legacy/projects/common/modules/analytics/foresee-survey.js

--- static/src/javascripts-legacy/projects/common/modules/analytics/foresee-survey.js
+++ static/src/javascripts-legacy/projects/common/modules/analytics/foresee-survey.js
@@ -1,39 +1,32 @@
-define([
-    'lib/config',
-    'lib/cookies',
-    'lib/detect',
-    'lib/load-script'
-], function (
-    config,
-    cookie,
-    detect,
-    loadScript
-    ) {
+import config from 'lib/config';
+import cookie from 'lib/cookies';
+import detect from 'lib/detect';
+import loadScript from 'lib/load-script';
 
-    function openForesee() {
-        loadScript.loadScript(config.libs.foresee);
+function openForesee() {
+    loadScript.loadScript(config.libs.foresee);
+}
+
+function load() {
+
+    var isNetworkFront = config.page.contentType === 'Network Front',
+        isProfilePage = config.page.contentType === 'userid',
+        sampleRate = detect.isBreakpoint({
+            max: 'mobile'
+        }) ? 0.008 : 0.006, // 0.8% mobile and 0.6% rest
+        sample = Math.random() <= sampleRate,
+        hasForcedOptIn = /forceForesee/.test(location.hash);
+
+    // the Foresee code is large, we only want to load it in when necessary.
+    if (!cookie.getCookie('GU_TEST') && !isNetworkFront && !isProfilePage && (window.openForeseeWhenReady || sample || hasForcedOptIn)) {
+        openForesee();
     }
 
-    function load() {
-
-        var isNetworkFront = config.page.contentType === 'Network Front',
-            isProfilePage = config.page.contentType === 'userid',
-            sampleRate = detect.isBreakpoint({max: 'mobile'}) ? 0.008 : 0.006, // 0.8% mobile and 0.6% rest
-            sample = Math.random() <= sampleRate,
-            hasForcedOptIn = /forceForesee/.test(location.hash);
-
-        // the Foresee code is large, we only want to load it in when necessary.
-        if (!cookie.getCookie('GU_TEST') && !isNetworkFront && !isProfilePage && (window.openForeseeWhenReady || sample || hasForcedOptIn)) {
-            openForesee();
-        }
-
-        if (window.guardian) {
-            window.guardian.openForesee = openForesee;
-        }
+    if (window.guardian) {
+        window.guardian.openForesee = openForesee;
     }
+}
 
-    return {
-        load: load
-    };
-
-});
+export default {
+    load: load
+};
