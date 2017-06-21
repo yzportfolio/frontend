5e899e465935b7397d6bf681a4bdc729faf990de
Alex Sanders <a@snd.rs>
2017-06-21 11:22:08 +0100

    convert static/src/javascripts-legacy/projects/membership/masker.js

--- static/src/javascripts-legacy/projects/membership/masker.js
+++ static/src/javascripts-legacy/projects/membership/masker.js
@@ -4,38 +4,35 @@
  *   el.addEventListener('keyup', maskInput(' / ', 2)) // date
  *   el.addEventListener('keyup', maskInput('', 4)) // cvc
  */
-define(function () {
-    function maskInput(delim, len) {
+function maskInput(delim, len) {
 
-        var tokRegex = new RegExp('\\d{1,' + len + '}', 'g'),
-            validRegex = new RegExp('\\d|(' + delim + ')|^', 'g'),
-            delimRegex = new RegExp(delim, 'g'),
-            prevValue = '';
+    var tokRegex = new RegExp('\\d{1,' + len + '}', 'g'),
+        validRegex = new RegExp('\\d|(' + delim + ')|^', 'g'),
+        delimRegex = new RegExp(delim, 'g'),
+        prevValue = '';
 
-        return function (event) {
-            if (event.keyCode !== 8) {
-                var toks = this.value.replace(delimRegex, '').match(tokRegex),
-                    value = '';
+    return function(event) {
+        if (event.keyCode !== 8) {
+            var toks = this.value.replace(delimRegex, '').match(tokRegex),
+                value = '';
 
-                if (toks && this.selectionEnd === this.value.length) {
-                    if (this.value.length >= prevValue.length
-                        && toks[toks.length - 1].length === len) {
-                        toks.push('');
-                    }
-
-                    value = toks.join(delim).slice(0, this.maxLength);
-                } else {
-                    value = this.value.match(validRegex).join('');
+            if (toks && this.selectionEnd === this.value.length) {
+                if (this.value.length >= prevValue.length && toks[toks.length - 1].length === len) {
+                    toks.push('');
                 }
 
-                if (value !== this.value) {
-                    this.value = value;
-                }
-
-                prevValue = this.value;
+                value = toks.join(delim).slice(0, this.maxLength);
+            } else {
+                value = this.value.match(validRegex).join('');
             }
-        };
-    }
 
-    return maskInput;
-});
+            if (value !== this.value) {
+                this.value = value;
+            }
+
+            prevValue = this.value;
+        }
+    };
+}
+
+export default maskInput;
