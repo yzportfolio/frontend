70af3c6523de2438d69e8b21ca392f0394eae10d
Alex Sanders <a@snd.rs>
2017-06-21 11:21:49 +0100

    convert static/src/javascripts-legacy/projects/common/modules/sport/football/match-info.js

--- static/src/javascripts-legacy/projects/common/modules/sport/football/match-info.js
+++ static/src/javascripts-legacy/projects/common/modules/sport/football/match-info.js
@@ -1,34 +1,26 @@
-define([
-    'lib/$',
-    'lib/fetch-json'
-], function (
-    $,
-    fetchJSON
-) {
+import fetchJSON from 'lib/fetch-json';
 
-    /**
-     * @param {Object} match
-     * @param {string} whosCalling (url)
-     */
-    var MatchInfo = function (match, whosCalling) {
-        this.endpoint += (match.id ? match.id : [match.date].concat(match.teams).join('/')) +
-                '.json?page=' + encodeURIComponent(whosCalling);
-    };
+/**
+ * @param {Object} match
+ * @param {string} whosCalling (url)
+ */
+var MatchInfo = function(match, whosCalling) {
+    this.endpoint += (match.id ? match.id : [match.date].concat(match.teams).join('/')) +
+        '.json?page=' + encodeURIComponent(whosCalling);
+};
 
-    /**
-     * @type {string}
-     */
-    MatchInfo.prototype.endpoint = '/football/api/match-nav/';
+/**
+ * @type {string}
+ */
+MatchInfo.prototype.endpoint = '/football/api/match-nav/';
 
-    /**
-     * @return Promise
-     */
-    MatchInfo.prototype.fetch = function () {
-        return fetchJSON(this.endpoint, {
-            mode: 'cors',
-        });
-    };
+/**
+ * @return Promise
+ */
+MatchInfo.prototype.fetch = function() {
+    return fetchJSON(this.endpoint, {
+        mode: 'cors',
+    });
+};
 
-    return MatchInfo;
-
-}); // define
+export default MatchInfo; // define
