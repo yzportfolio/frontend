d78a195086290a27411199589ed91c031b5679a1
Alex Sanders <a@snd.rs>
2017-06-21 11:21:50 +0100

    convert static/src/javascripts-legacy/projects/common/modules/sport/football/tag-page-stats.js

--- static/src/javascripts-legacy/projects/common/modules/sport/football/tag-page-stats.js
+++ static/src/javascripts-legacy/projects/common/modules/sport/football/tag-page-stats.js
@@ -1,27 +1,22 @@
-define([
-    'lib/config',
-    'lib/fetch-json',
-    'lib/report-error'
-], function (
-    config,
-    fetchJson,
-    reportError
-) {
-    return function () {
-        var firstContainer = document.querySelector('.js-insert-team-stats-after');
+import config from 'lib/config';
+import fetchJson from 'lib/fetch-json';
+import reportError from 'lib/report-error';
+export default function() {
+    var firstContainer = document.querySelector('.js-insert-team-stats-after');
 
-        if (firstContainer) {
-            fetchJson('/' + config.page.pageId + '/fixtures-and-results-container', {
+    if (firstContainer) {
+        fetchJson('/' + config.page.pageId + '/fixtures-and-results-container', {
                 mode: 'cors'
             })
-            .then(function (container) {
+            .then(function(container) {
                 if (container.html) {
                     firstContainer.insertAdjacentHTML('afterend', container.html);
                 }
             })
-            .catch(function (ex) {
-                reportError(ex, { feature: 'tag-fixtures' });
+            .catch(function(ex) {
+                reportError(ex, {
+                    feature: 'tag-fixtures'
+                });
             });
-        }
-    };
-});
+    }
+}
