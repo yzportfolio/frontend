17935d66cf72f9394b2577f0127901a8f40b8950
Alex Sanders <a@snd.rs>
2017-06-21 11:20:39 +0100

    convert static/src/javascripts-legacy/projects/common/modules/analytics/beacon.js

--- static/src/javascripts-legacy/projects/common/modules/analytics/beacon.js
+++ static/src/javascripts-legacy/projects/common/modules/analytics/beacon.js
@@ -1,28 +1,22 @@
-define([
-    'lib/config',
-    'lib/fetch',
-], function (
-    config,
-    fetch
-) {
-    return {
-        fire: function (path) {
-            var img = new Image();
-            img.src = config.page.beaconUrl + path;
+import config from 'lib/config';
+import fetch from 'lib/fetch';
+export default {
+    fire: function(path) {
+        var img = new Image();
+        img.src = config.page.beaconUrl + path;
 
-            return img;
-        },
-        postJson: function (path, jsonString) {
-            var url = (config.page.beaconUrl || '').replace(/^\/\//, window.location.protocol + '//') + path;
+        return img;
+    },
+    postJson: function(path, jsonString) {
+        var url = (config.page.beaconUrl || '').replace(/^\/\//, window.location.protocol + '//') + path;
 
-            fetch(url, {
-                method: 'post',
-                header: {
-                    'Content-Type': 'application/json',
-                },
-                body: jsonString,
-                mode: 'cors',
-            });
-        },
-    };
-});
+        fetch(url, {
+            method: 'post',
+            header: {
+                'Content-Type': 'application/json',
+            },
+            body: jsonString,
+            mode: 'cors',
+        });
+    },
+};
