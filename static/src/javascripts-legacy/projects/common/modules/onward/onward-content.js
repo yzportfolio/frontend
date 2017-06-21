6d9b0b2f77badf680b6d9a08934f152a40e43a2d
Alex Sanders <a@snd.rs>
2017-06-21 11:21:43 +0100

    convert static/src/javascripts-legacy/projects/common/modules/onward/onward-content.js

--- static/src/javascripts-legacy/projects/common/modules/onward/onward-content.js
+++ static/src/javascripts-legacy/projects/common/modules/onward/onward-content.js
@@ -1,42 +1,32 @@
-define([
-    'lib/config',
-    'lib/mediator',
-    'common/modules/analytics/register',
-    'common/modules/component',
-    'lodash/arrays/union'
-], function (
-    config,
-    mediator,
-    register,
-    Component,
-    union
-) {
+import config from 'lib/config';
+import mediator from 'lib/mediator';
+import register from 'common/modules/analytics/register';
+import Component from 'common/modules/component';
+import union from 'lodash/arrays/union';
 
-    var getTag = function () {
-        var seriesAndBlogTags = config.page.blogIds.split(',').concat([config.page.seriesId]);
-        return union(config.page.nonKeywordTagIds.split(','), seriesAndBlogTags).shift();
-    };
+var getTag = function() {
+    var seriesAndBlogTags = config.page.blogIds.split(',').concat([config.page.seriesId]);
+    return union(config.page.nonKeywordTagIds.split(','), seriesAndBlogTags).shift();
+};
 
-    function OnwardContent(context) {
-        register.begin('series-content');
-        this.context = context;
-        this.endpoint = '/series/' + getTag() + '.json?shortUrl=' + encodeURIComponent(config.page.shortUrl);
-        this.fetch(this.context, 'html');
-    }
+function OnwardContent(context) {
+    register.begin('series-content');
+    this.context = context;
+    this.endpoint = '/series/' + getTag() + '.json?shortUrl=' + encodeURIComponent(config.page.shortUrl);
+    this.fetch(this.context, 'html');
+}
 
-    Component.define(OnwardContent);
+Component.define(OnwardContent);
 
-    OnwardContent.prototype.ready = function () {
-        register.end('series-content');
-        mediator.emit('modules:onward:loaded');
-        mediator.emit('page:new-content');
-        mediator.emit('ui:images:upgradePictures');
-    };
+OnwardContent.prototype.ready = function() {
+    register.end('series-content');
+    mediator.emit('modules:onward:loaded');
+    mediator.emit('page:new-content');
+    mediator.emit('ui:images:upgradePictures');
+};
 
-    OnwardContent.prototype.error = function () {
-        register.error('series-content');
-    };
+OnwardContent.prototype.error = function() {
+    register.error('series-content');
+};
 
-    return OnwardContent;
-
-});
+export default OnwardContent;
