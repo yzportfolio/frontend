a213a09e4c64cd26eec8a1c7c1f6cf870b0a216b
Alex Sanders <a@snd.rs>
2017-06-21 11:21:41 +0100

    convert static/src/javascripts-legacy/projects/common/modules/navigation/user-account.js

--- static/src/javascripts-legacy/projects/common/modules/navigation/user-account.js
+++ static/src/javascripts-legacy/projects/common/modules/navigation/user-account.js
@@ -1,55 +1,48 @@
-define([
-    'fastdom',
-    'common/modules/identity/api',
-    'lib/config',
-], function (
-    fastdom,
-    id,
-    config
-) {
+import fastdom from 'fastdom';
+import id from 'common/modules/identity/api';
+import config from 'lib/config';
 
-    function updateCommentLink () {
-        var commentItem = document.querySelector('.js-show-comment-activity');
-        var commentLink = commentItem && commentItem.querySelector('.js-add-comment-activity-link');
+function updateCommentLink() {
+    var commentItem = document.querySelector('.js-show-comment-activity');
+    var commentLink = commentItem && commentItem.querySelector('.js-add-comment-activity-link');
 
-        if (commentItem && commentLink) {
-            var user = id.getUserFromCookie();
+    if (commentItem && commentLink) {
+        var user = id.getUserFromCookie();
 
-            fastdom.write(function() {
-                commentItem.classList.remove('u-h');
-                commentLink.setAttribute('href', 'https://profile.theguardian.com/user/id/' + user.id);
-            });
-        }
+        fastdom.write(function() {
+            commentItem.classList.remove('u-h');
+            commentLink.setAttribute('href', 'https://profile.theguardian.com/user/id/' + user.id);
+        });
     }
+}
 
-    function showMyAccountIfNecessary () {
-        if(id.isUserLoggedIn()) {
-            var signIn = document.querySelector('.js-navigation-sign-in');
-            var accountDetails = document.querySelector('.js-navigation-account-details');
-            var accountActions = document.querySelector('.js-navigation-account-actions');
-            var user = config.user;
-            var userNameEl = document.querySelector('.js-navigation-account-username');
+function showMyAccountIfNecessary() {
+    if (id.isUserLoggedIn()) {
+        var signIn = document.querySelector('.js-navigation-sign-in');
+        var accountDetails = document.querySelector('.js-navigation-account-details');
+        var accountActions = document.querySelector('.js-navigation-account-actions');
+        var user = config.user;
+        var userNameEl = document.querySelector('.js-navigation-account-username');
 
-            fastdom.write(function() {
-                if (signIn) {
-                    signIn.classList.add('u-h');
-                }
+        fastdom.write(function() {
+            if (signIn) {
+                signIn.classList.add('u-h');
+            }
 
-                if (accountDetails) {
-                    accountDetails.classList.remove('u-h');
-                }
+            if (accountDetails) {
+                accountDetails.classList.remove('u-h');
+            }
 
-                if (accountActions) {
-                    accountActions.classList.remove('u-h');
-                    updateCommentLink();
-                }
+            if (accountActions) {
+                accountActions.classList.remove('u-h');
+                updateCommentLink();
+            }
 
-                if (userNameEl && user && user.displayName) {
-                    userNameEl.innerText = user.displayName;
-                }
-            });
-        }
+            if (userNameEl && user && user.displayName) {
+                userNameEl.innerText = user.displayName;
+            }
+        });
     }
+}
 
-    return showMyAccountIfNecessary;
-});
+export default showMyAccountIfNecessary;
