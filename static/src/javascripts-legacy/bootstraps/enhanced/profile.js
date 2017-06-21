3804bbc762c599b5eb0147c9a6a530952cc862e6
Alex Sanders <a@snd.rs>
2017-06-21 11:12:22 +0100

    convert module

--- static/src/javascripts-legacy/bootstraps/enhanced/profile.js
+++ static/src/javascripts-legacy/bootstraps/enhanced/profile.js
@@ -1,126 +1,106 @@
-define([
-    'lib/$',
-    'qwery',
-    'common/modules/identity/forms',
-    'common/modules/identity/formstack', // oldskool inside
-    'common/modules/identity/formstack-iframe', // oldskool outside
-    'common/modules/identity/formstack-iframe-embed', // newskool inside
-    'common/modules/identity/validation-email',
-    'common/modules/identity/api',
-    'common/modules/identity/account-profile',
-    'common/modules/identity/public-profile',
-    'common/modules/identity/email-preferences',
-    'common/modules/identity/delete-account',
-    'common/modules/discussion/user-avatars',
-    'lib/mediator',
-    'common/modules/ui/tabs'
-], function (
-    $,
-    qwery,
-    Identity,
-    Formstack,
-    FormstackIframe,
-    FormstackEmbedIframe,
-    ValidationEmail,
-    Id,
-    AccountProfile,
-    PublicProfile,
-    EmailPreferences,
-    DeleteAccount,
-    UserAvatars,
-    mediator,
-    Tabs
-) {
-    var modules = {
-        initFormstack: function () {
-            mediator.on('page:identity:ready', function (config) {
-                var attr = 'data-formstack-id';
-                $('[' + attr + ']').each(function (el) {
-                    var id = el.getAttribute(attr),
-                        isEmbed = el.className.match(/\bformstack-embed\b/);
+import $ from 'lib/$';
+import Identity from 'common/modules/identity/forms';
+import Formstack from 'common/modules/identity/formstack';
+import FormstackIframe from 'common/modules/identity/formstack-iframe';
+import FormstackEmbedIframe from 'common/modules/identity/formstack-iframe-embed';
+import ValidationEmail from 'common/modules/identity/validation-email';
+import AccountProfile from 'common/modules/identity/account-profile';
+import PublicProfile from 'common/modules/identity/public-profile';
+import EmailPreferences from 'common/modules/identity/email-preferences';
+import DeleteAccount from 'common/modules/identity/delete-account';
+import UserAvatars from 'common/modules/discussion/user-avatars';
+import mediator from 'lib/mediator';
+import Tabs from 'common/modules/ui/tabs';
 
-                    if (isEmbed) {
-                        new FormstackEmbedIframe(el, id, config).init();
-                    } else {
-                        new Formstack(el, id, config).init();
-                    }
+var modules = {
+    initFormstack: function() {
+        mediator.on('page:identity:ready', function(config) {
+            var attr = 'data-formstack-id';
+            $('[' + attr + ']').each(function(el) {
+                var id = el.getAttribute(attr),
+                    isEmbed = el.className.match(/\bformstack-embed\b/);
 
-                });
+                if (isEmbed) {
+                    new FormstackEmbedIframe(el, id, config).init();
+                } else {
+                    new Formstack(el, id, config).init();
+                }
 
-                // Load old js if necessary
-                $('.js-formstack-iframe').each(function (el) {
-                    new FormstackIframe(el, config).init();
-                });
             });
-        },
-        forgottenEmail: function () {
-            mediator.on('page:identity:ready', function (config) {
-                Identity.forgottenEmail(config);
-            });
-        },
-        forgottenPassword: function () {
-            mediator.on('page:identity:ready', function (config) {
-                Identity.forgottenPassword(config);
-            });
-        },
-        passwordToggle: function () {
-            mediator.on('page:identity:ready', function (config) {
-                Identity.passwordToggle(config);
-            });
-        },
-        userAvatars: function () {
-            mediator.on('page:identity:ready', function () {
-                UserAvatars.init();
-            });
-        },
-        validationEmail: function () {
-            mediator.on('page:identity:ready', function () {
-                ValidationEmail.init();
-            });
-        },
 
-        tabs: function () {
-            var tabs = new Tabs();
-            mediator.on('page:identity:ready', function () {
-                tabs.init();
+            // Load old js if necessary
+            $('.js-formstack-iframe').each(function(el) {
+                new FormstackIframe(el, config).init();
             });
-        },
+        });
+    },
+    forgottenEmail: function() {
+        mediator.on('page:identity:ready', function(config) {
+            Identity.forgottenEmail(config);
+        });
+    },
+    forgottenPassword: function() {
+        mediator.on('page:identity:ready', function(config) {
+            Identity.forgottenPassword(config);
+        });
+    },
+    passwordToggle: function() {
+        mediator.on('page:identity:ready', function(config) {
+            Identity.passwordToggle(config);
+        });
+    },
+    userAvatars: function() {
+        mediator.on('page:identity:ready', function() {
+            UserAvatars.init();
+        });
+    },
+    validationEmail: function() {
+        mediator.on('page:identity:ready', function() {
+            ValidationEmail.init();
+        });
+    },
 
-        accountProfile: function () {
-            var accountProfile = new AccountProfile();
-            mediator.on('page:identity:ready', function () {
-                accountProfile.init();
-            });
-        },
+    tabs: function() {
+        var tabs = new Tabs();
+        mediator.on('page:identity:ready', function() {
+            tabs.init();
+        });
+    },
 
-        emailPreferences: function () {
-            mediator.on('page:identity:ready', function () {
-                EmailPreferences.init();
-            });
-        },
+    accountProfile: function() {
+        var accountProfile = new AccountProfile();
+        mediator.on('page:identity:ready', function() {
+            accountProfile.init();
+        });
+    },
 
-        deleteAccount: function () {
-            mediator.on('page:identity:ready', function () {
-                DeleteAccount.init();
-            });
-        }
-    };
+    emailPreferences: function() {
+        mediator.on('page:identity:ready', function() {
+            EmailPreferences.init();
+        });
+    },
 
-    return {
-        init: function (config) {
-            modules.initFormstack();
-            modules.forgottenEmail();
-            modules.forgottenPassword();
-            modules.passwordToggle();
-            modules.userAvatars();
-            modules.validationEmail();
-            modules.tabs();
-            modules.accountProfile();
-            modules.emailPreferences();
-            modules.deleteAccount();
-            PublicProfile.init();
+    deleteAccount: function() {
+        mediator.on('page:identity:ready', function() {
+            DeleteAccount.init();
+        });
+    }
+};
 
-            mediator.emit('page:identity:ready', config);
-        }
-    };
-});
+export default {
+    init: function(config) {
+        modules.initFormstack();
+        modules.forgottenEmail();
+        modules.forgottenPassword();
+        modules.passwordToggle();
+        modules.userAvatars();
+        modules.validationEmail();
+        modules.tabs();
+        modules.accountProfile();
+        modules.emailPreferences();
+        modules.deleteAccount();
+        PublicProfile.init();
+
+        mediator.emit('page:identity:ready', config);
+    }
+};
