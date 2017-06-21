b5157ddcc99deed7272ba5a5a01860036a2187f0
Alex Sanders <a@snd.rs>
2017-06-21 11:20:25 +0100

    convert static/src/javascripts-legacy/lib/clamp.js

--- static/src/javascripts-legacy/lib/clamp.js
+++ static/src/javascripts-legacy/lib/clamp.js
@@ -1,43 +1,36 @@
-define([
-    'bonzo',
-    'bean'
-], function (
-    bonzo,
-    bean
-) {
+import bonzo from 'bonzo';
+import bean from 'bean';
 
-    var clamp = function (elem, lines, showMore) {
-        var height = elem.clientHeight,
-            lineHeight = getComputedStyle(elem).getPropertyValue('line-height'),
-            maxHeight = (parseInt(lineHeight, 10) + (showMore ? 2 : 0)) * lines,
-            $fade = bonzo(bonzo.create('<span class="clamp-fade"></span>')),
-            $elem = bonzo(elem),
-            $showMore;
+var clamp = function(elem, lines, showMore) {
+    var height = elem.clientHeight,
+        lineHeight = getComputedStyle(elem).getPropertyValue('line-height'),
+        maxHeight = (parseInt(lineHeight, 10) + (showMore ? 2 : 0)) * lines,
+        $fade = bonzo(bonzo.create('<span class="clamp-fade"></span>')),
+        $elem = bonzo(elem),
+        $showMore;
 
-        if (height < maxHeight) {
-            return;
-        }
+    if (height < maxHeight) {
+        return;
+    }
 
-        $elem.css({
-            maxHeight: maxHeight + 'px',
-            overflow: 'hidden'
-        });
+    $elem.css({
+        maxHeight: maxHeight + 'px',
+        overflow: 'hidden'
+    });
 
-        $elem.after($fade);
+    $elem.after($fade);
 
-        if (showMore) {
-            $showMore = bonzo(bonzo.create('<span class="clamp-fade__content u-fauxlink" role="button">Read more</span>'));
-            $fade.append($showMore);
-            bean.on($showMore[0], 'click', function () {
-                $fade.remove();
-                $elem.css({
-                    maxHeight: 'none',
-                    overflow: 'auto'
-                });
+    if (showMore) {
+        $showMore = bonzo(bonzo.create('<span class="clamp-fade__content u-fauxlink" role="button">Read more</span>'));
+        $fade.append($showMore);
+        bean.on($showMore[0], 'click', function() {
+            $fade.remove();
+            $elem.css({
+                maxHeight: 'none',
+                overflow: 'auto'
             });
-        }
-    };
+        });
+    }
+};
 
-    return clamp;
-
-}); // define
+export default clamp; // define
