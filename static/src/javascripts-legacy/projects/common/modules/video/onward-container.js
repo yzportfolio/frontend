bcc1dbb5d544b5d708aabeb4c3e38c900d18f326
Alex Sanders <a@snd.rs>
2017-06-21 11:22:03 +0100

    convert static/src/javascripts-legacy/projects/common/modules/video/onward-container.js

--- static/src/javascripts-legacy/projects/common/modules/video/onward-container.js
+++ static/src/javascripts-legacy/projects/common/modules/video/onward-container.js
@@ -1,57 +1,50 @@
-define([
-    'bean',
-    'lib/config',
-    'common/modules/component',
-    'lib/mediator'
-], function(
-    bean,
-    config,
-    Component,
-    mediator
-) {
+import bean from 'bean';
+import config from 'lib/config';
+import Component from 'common/modules/component';
+import mediator from 'lib/mediator';
 
-    function getEndpoint(mediaType) {
-        var isInSeries = Boolean(config.page.seriesTags);
+function getEndpoint(mediaType) {
+    var isInSeries = Boolean(config.page.seriesTags);
 
-        if (isInSeries) {
-            return '/video/in-series/' + config.page.seriesId + '.json';
-        } else {
-            return '/' + (config.page.isPodcast ? 'podcast' : mediaType) + '/most-viewed.json';
-        }
+    if (isInSeries) {
+        return '/video/in-series/' + config.page.seriesId + '.json';
+    } else {
+        return '/' + (config.page.isPodcast ? 'podcast' : mediaType) + '/most-viewed.json';
     }
+}
 
-    function initEvents(el, manipulationType, endpoint) {
-        bean.on(el, 'click', '.most-viewed-navigation__button', function(ev) {
-            var page = ev.currentTarget.getAttribute('data-page');
+function initEvents(el, manipulationType, endpoint) {
+    bean.on(el, 'click', '.most-viewed-navigation__button', function(ev) {
+        var page = ev.currentTarget.getAttribute('data-page');
 
-            createComponent(el, endpoint, manipulationType, page);
+        createComponent(el, endpoint, manipulationType, page);
 
-            ev.preventDefault();
-            return false;
-        });
-    }
+        ev.preventDefault();
+        return false;
+    });
+}
 
-    function createComponent(el, endpoint, manipulationType, page) {
-        var component = new Component();
-        var paginatedEndpoint = endpoint + (page ? '?page=' + page : '');
-        component.manipulationType = manipulationType;
-        component.endpoint = paginatedEndpoint;
+function createComponent(el, endpoint, manipulationType, page) {
+    var component = new Component();
+    var paginatedEndpoint = endpoint + (page ? '?page=' + page : '');
+    component.manipulationType = manipulationType;
+    component.endpoint = paginatedEndpoint;
 
-        el.innerHTML = ''; // we have no replace in component
+    el.innerHTML = ''; // we have no replace in component
 
-        return component.fetch(el, 'html');
-    }
+    return component.fetch(el, 'html');
+}
 
-    function init(el, mediaType) {
-        var manipulationType = mediaType === 'video' ? 'append' : 'html';
-        var endpoint = getEndpoint(mediaType);
+function init(el, mediaType) {
+    var manipulationType = mediaType === 'video' ? 'append' : 'html';
+    var endpoint = getEndpoint(mediaType);
 
-        createComponent(el, endpoint, manipulationType).then(function() {
-            mediator.emit('page:new-content');
-            initEvents(el, manipulationType, endpoint);
-        });
-    }
+    createComponent(el, endpoint, manipulationType).then(function() {
+        mediator.emit('page:new-content');
+        initEvents(el, manipulationType, endpoint);
+    });
+}
 
-    return { init: init };
-
-});
+export default {
+    init: init
+};
