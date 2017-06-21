24c2036639131167167b1141f2a54df4f7edb4b3
Alex Sanders <a@snd.rs>
2017-06-21 11:21:11 +0100

    convert static/src/javascripts-legacy/projects/common/modules/discussion/user-avatars.js

--- static/src/javascripts-legacy/projects/common/modules/discussion/user-avatars.js
+++ static/src/javascripts-legacy/projects/common/modules/discussion/user-avatars.js
@@ -1,49 +1,49 @@
-define([
-    'lib/$',
-    'bonzo',
-    'common/modules/avatar/api',
-    'lib/config'
-], function ($, bonzo, avatarApi, config) {
+import $ from 'lib/$';
+import bonzo from 'bonzo';
+import avatarApi from 'common/modules/avatar/api';
+import config from 'lib/config';
 
-    function init() {
-        $('.user-avatar').each(avatarify);
+function init() {
+    $('.user-avatar').each(avatarify);
+}
+
+function avatarify(el) {
+    var container = bonzo(el),
+        updating = bonzo(bonzo.create('<div class="is-updating"></div>')),
+        avatar = bonzo(bonzo.create('<img class="user-avatar__image" alt="" />')),
+        avatarUserId = container.data('userid'),
+        userId = config.user ? parseInt(config.user.id) : null,
+        ownAvatar = avatarUserId === userId;
+
+    var updateCleanup = function() {
+        updating.remove();
+        avatar.appendTo(container);
+    };
+
+    container
+        .removeClass('is-hidden');
+
+    updating
+        .css('display', 'block')
+        .appendTo(container);
+
+    if (ownAvatar) {
+        avatarApi.getActive()
+            .then(function(response) {
+                avatar.attr('src', response.data.avatarUrl);
+            }, function() {
+                avatar.attr('src', avatarApi.deterministicUrl(avatarUserId));
+            })
+            .always(function() {
+                updateCleanup();
+            });
+    } else {
+        avatar.attr('src', avatarApi.deterministicUrl(avatarUserId));
+        updateCleanup();
     }
+}
 
-    function avatarify(el) {
-        var container = bonzo(el),
-            updating = bonzo(bonzo.create('<div class="is-updating"></div>')),
-            avatar = bonzo(bonzo.create('<img class="user-avatar__image" alt="" />')),
-            avatarUserId = container.data('userid'),
-            userId = config.user ? parseInt(config.user.id) : null,
-            ownAvatar = avatarUserId === userId;
-
-        var updateCleanup = function () {
-            updating.remove();
-            avatar.appendTo(container);
-        };
-
-        container
-            .removeClass('is-hidden');
-
-        updating
-            .css('display', 'block')
-            .appendTo(container);
-
-        if (ownAvatar) {
-            avatarApi.getActive()
-                .then(function (response) {
-                    avatar.attr('src', response.data.avatarUrl);
-                }, function () {
-                    avatar.attr('src', avatarApi.deterministicUrl(avatarUserId));
-                })
-                .always(function () {
-                    updateCleanup();
-                });
-        } else {
-            avatar.attr('src', avatarApi.deterministicUrl(avatarUserId));
-            updateCleanup();
-        }
-    }
-
-    return {init: init, avatarify: avatarify};
-});
+export default {
+    init: init,
+    avatarify: avatarify
+};
