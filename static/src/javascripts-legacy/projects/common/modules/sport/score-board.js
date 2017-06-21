7646b20c560d3ed9a537ac408353f59c1f2115c0
Alex Sanders <a@snd.rs>
2017-06-21 11:21:51 +0100

    convert static/src/javascripts-legacy/projects/common/modules/sport/score-board.js

--- static/src/javascripts-legacy/projects/common/modules/sport/score-board.js
+++ static/src/javascripts-legacy/projects/common/modules/sport/score-board.js
@@ -1,77 +1,68 @@
-define([
-    'bonzo',
-    'lib/detect',
-    'lib/$',
-    'lodash/utilities/template',
-    'common/modules/component',
-    'raw-loader!common/views/sport/score-container.html'
-], function (
-    bonzo,
-    detect,
-    $,
-    template,
-    component,
-    scoreContainerHtml
-) {
-    /* Parameters:
-        - context.pageType          - one of 'minbymin', 'preview', 'report', 'stats'
-        - context.parent            - the bonzo element that the placeholder, and eventually score, will attach to.
-        - context.autoupdated       - true if the scoreboard should update every 30 or 60 seconds, depending on device.
-        - context.responseDataKey   - the key of the ajax-loaded json object that maps to the scoreboard html.
-        - context.endpoint          - the ajax endpoint which will provide html scores inside a json object.
-    */
-    function ScoreBoard(context) {
-        this.pageType = context.pageType;
-        this.parent = context.parent;
+import bonzo from 'bonzo';
+import detect from 'lib/detect';
+import $ from 'lib/$';
+import template from 'lodash/utilities/template';
+import component from 'common/modules/component';
+import scoreContainerHtml from 'raw-loader!common/views/sport/score-container.html';
+/* Parameters:
+    - context.pageType          - one of 'minbymin', 'preview', 'report', 'stats'
+    - context.parent            - the bonzo element that the placeholder, and eventually score, will attach to.
+    - context.autoupdated       - true if the scoreboard should update every 30 or 60 seconds, depending on device.
+    - context.responseDataKey   - the key of the ajax-loaded json object that maps to the scoreboard html.
+    - context.endpoint          - the ajax endpoint which will provide html scores inside a json object.
+*/
+function ScoreBoard(context) {
+    this.pageType = context.pageType;
+    this.parent = context.parent;
 
-        this.placeholder = bonzo.create(template(scoreContainerHtml, {
-            loadingState: this.pageType !== 'report' ? ' score__loading--live' : ''
-        }))[0];
+    this.placeholder = bonzo.create(template(scoreContainerHtml, {
+        loadingState: this.pageType !== 'report' ? ' score__loading--live' : ''
+    }))[0];
 
-        if (this.pageType === 'report') {
-            context.parent.after(this.placeholder);
-        } else {
-            context.parent.addClass('u-h').before(this.placeholder);
-        }
-
-        // These parameters configure the way the Component super class fetches new score html.
-        this.endpoint = context.endpoint;
-        this.autoupdated = context.autoupdated;
-        this.responseDataKey = context.responseDataKey;
-        this.updateEvery = detect.isBreakpoint({ min: 'desktop' }) ? 30 : 60;
+    if (this.pageType === 'report') {
+        context.parent.after(this.placeholder);
+    } else {
+        context.parent.addClass('u-h').before(this.placeholder);
     }
 
-    component.define(ScoreBoard);
+    // These parameters configure the way the Component super class fetches new score html.
+    this.endpoint = context.endpoint;
+    this.autoupdated = context.autoupdated;
+    this.responseDataKey = context.responseDataKey;
+    this.updateEvery = detect.isBreakpoint({
+        min: 'desktop'
+    }) ? 30 : 60;
+}
 
-    ScoreBoard.prototype.componentClass = 'match-summary';
+component.define(ScoreBoard);
 
-    ScoreBoard.prototype.prerender = function () {
-        var scoreLoadingPlaceholder = $('.score__loading', $(this.placeholder));
-        if (scoreLoadingPlaceholder.length) {
-            scoreLoadingPlaceholder.remove();
-        }
-    };
+ScoreBoard.prototype.componentClass = 'match-summary';
 
-    ScoreBoard.prototype.ready = function () {
-        this.setState(this.pageType);
-    };
+ScoreBoard.prototype.prerender = function() {
+    var scoreLoadingPlaceholder = $('.score__loading', $(this.placeholder));
+    if (scoreLoadingPlaceholder.length) {
+        scoreLoadingPlaceholder.remove();
+    }
+};
 
-    // Load the first score html component from ajax, and any subsequent score updates from the same endpoint.
-    ScoreBoard.prototype.load = function () {
-        this.fetch(this.placeholder);
-    };
+ScoreBoard.prototype.ready = function() {
+    this.setState(this.pageType);
+};
 
-    // Load the first score html component from json, and any subsequent score updates from ajax.
-    ScoreBoard.prototype.loadFromJson = function (html) {
-        this.template = html;
-        this.render(this.placeholder);
-    };
+// Load the first score html component from ajax, and any subsequent score updates from the same endpoint.
+ScoreBoard.prototype.load = function() {
+    this.fetch(this.placeholder);
+};
 
-    ScoreBoard.prototype.error = function () {
-        this.placeholder.innerHTML = '';
-        this.parent.removeClass('u-h');
-    };
+// Load the first score html component from json, and any subsequent score updates from ajax.
+ScoreBoard.prototype.loadFromJson = function(html) {
+    this.template = html;
+    this.render(this.placeholder);
+};
 
-    return ScoreBoard;
+ScoreBoard.prototype.error = function() {
+    this.placeholder.innerHTML = '';
+    this.parent.removeClass('u-h');
+};
 
-}); // define
+export default ScoreBoard; // define
