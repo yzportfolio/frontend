63d1909a0d4af8d1a52493beb78f6d56988ea89a
Alex Sanders <a@snd.rs>
2017-06-21 11:21:02 +0100

    convert static/src/javascripts-legacy/projects/common/modules/crosswords/hidden-input.js

--- static/src/javascripts-legacy/projects/common/modules/crosswords/hidden-input.js
+++ static/src/javascripts-legacy/projects/common/modules/crosswords/hidden-input.js
@@ -1,68 +1,58 @@
-define([
-    'react/addons',
-    'bonzo',
-    'fastdom',
-    'lib/$',
-    'lib/scroller',
-    'lib/detect'
-], function (
-    React,
-    bonzo,
-    fastdom,
-    $,
-    scroller,
-    detect
-) {
-    var HiddenInput = React.createClass({
+import React from 'react/addons';
+import bonzo from 'bonzo';
+import fastdom from 'fastdom';
+import $ from 'lib/$';
+import scroller from 'lib/scroller';
+import detect from 'lib/detect';
+var HiddenInput = React.createClass({
 
-        getInitialState: function () {
-            return {
-                value: this.props.value
-            };
-        },
+    getInitialState: function() {
+        return {
+            value: this.props.value
+        };
+    },
 
-        componentDidUpdate: function () {
-            if (detect.isBreakpoint({
-                    max: 'mobile'
-                })) {
-                fastdom.read(function () {
-                    var offsets = bonzo(React.findDOMNode(this.refs.input)).offset();
-                    scroller.scrollTo(offsets.top - offsets.height * 1.5 - $('.crossword__sticky-clue').offset().height, 250, 'easeOutQuad');
-                }.bind(this));
-            }
-        },
-
-        handleChange: function (event) {
-            this.props.onChange(event.target.value.toUpperCase());
-            this.setState({
-                value: ''
-            });
-        },
-
-        render: function () {
-            return React.createElement(
-                'div', {
-                    className: 'crossword__hidden-input-wrapper',
-                    ref: 'wrapper'
-                },
-                React.createElement('input', {
-                    type: 'text',
-                    className: 'crossword__hidden-input',
-                    maxLength: '1',
-                    onClick: this.props.onClick,
-                    onChange: this.handleChange,
-                    onTouchStart: this.props.touchStart,
-                    onKeyDown: this.props.onKeyDown,
-                    onBlur: this.props.onBlur,
-                    value: this.state.value,
-                    autoComplete: 'off',
-                    spellCheck: 'false',
-                    autoCorrect: 'off',
-                    ref: 'input'
-                })
-            );
+    componentDidUpdate: function() {
+        if (detect.isBreakpoint({
+                max: 'mobile'
+            })) {
+            fastdom.read(function() {
+                var offsets = bonzo(React.findDOMNode(this.refs.input)).offset();
+                scroller.scrollTo(offsets.top - offsets.height * 1.5 - $('.crossword__sticky-clue').offset().height, 250, 'easeOutQuad');
+            }.bind(this));
         }
-    });
+    },
 
-    return HiddenInput;
+    handleChange: function(event) {
+        this.props.onChange(event.target.value.toUpperCase());
+        this.setState({
+            value: ''
+        });
+    },
+
+    render: function() {
+        return React.createElement(
+            'div', {
+                className: 'crossword__hidden-input-wrapper',
+                ref: 'wrapper'
+            },
+            React.createElement('input', {
+                type: 'text',
+                className: 'crossword__hidden-input',
+                maxLength: '1',
+                onClick: this.props.onClick,
+                onChange: this.handleChange,
+                onTouchStart: this.props.touchStart,
+                onKeyDown: this.props.onKeyDown,
+                onBlur: this.props.onBlur,
+                value: this.state.value,
+                autoComplete: 'off',
+                spellCheck: 'false',
+                autoCorrect: 'off',
+                ref: 'input'
+            })
+        );
+    }
 });
+
+export default HiddenInput;
