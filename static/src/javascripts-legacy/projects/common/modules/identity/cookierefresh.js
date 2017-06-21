1536d75162221bf9f9e2cc48ee93bdebf7ebebb4
Alex Sanders <a@snd.rs>
2017-06-21 11:21:30 +0100

    convert static/src/javascripts-legacy/projects/common/modules/identity/cookierefresh.js

--- static/src/javascripts-legacy/projects/common/modules/identity/cookierefresh.js
+++ static/src/javascripts-legacy/projects/common/modules/identity/cookierefresh.js
@@ -1,26 +1,20 @@
-define([
-    'common/modules/identity/api',
-    'lib/storage'
-],
-function (
-    Id,
-    storage
-) {
-    function CookieRefresh() {
-        this.init = function () {
-            if (storage.local.isAvailable() && Id.isUserLoggedIn()) {
-                var lastRefresh = storage.local.get(Id.lastRefreshKey),
-                    currentTime = new Date().getTime();
-                if (this.shouldRefreshCookie(lastRefresh, currentTime)) {
-                    Id.getUserFromApiWithRefreshedCookie();
-                    storage.local.set(Id.lastRefreshKey, currentTime);
-                }
-            }
-        };
+import Id from 'common/modules/identity/api';
+import storage from 'lib/storage';
 
-        CookieRefresh.prototype.shouldRefreshCookie = function (lastRefresh, currentTime) {
-            return (!lastRefresh) || (currentTime > (parseInt(lastRefresh, 10) + (1000 * 86400 * 30)));
-        };
-    }
-    return CookieRefresh;
-});
+function CookieRefresh() {
+    this.init = function() {
+        if (storage.local.isAvailable() && Id.isUserLoggedIn()) {
+            var lastRefresh = storage.local.get(Id.lastRefreshKey),
+                currentTime = new Date().getTime();
+            if (this.shouldRefreshCookie(lastRefresh, currentTime)) {
+                Id.getUserFromApiWithRefreshedCookie();
+                storage.local.set(Id.lastRefreshKey, currentTime);
+            }
+        }
+    };
+
+    CookieRefresh.prototype.shouldRefreshCookie = function(lastRefresh, currentTime) {
+        return (!lastRefresh) || (currentTime > (parseInt(lastRefresh, 10) + (1000 * 86400 * 30)));
+    };
+}
+export default CookieRefresh;
