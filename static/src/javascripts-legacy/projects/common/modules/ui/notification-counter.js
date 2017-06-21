1cbabd387728de3f34a4732293432eae2bd74493
Alex Sanders <a@snd.rs>
2017-06-21 11:21:58 +0100

    convert static/src/javascripts-legacy/projects/common/modules/ui/notification-counter.js

--- static/src/javascripts-legacy/projects/common/modules/ui/notification-counter.js
+++ static/src/javascripts-legacy/projects/common/modules/ui/notification-counter.js
@@ -1,34 +1,29 @@
-define([
-    'lib/mediator'
-], function (
-    mediator
-) {
-    'use strict';
+import mediator from 'lib/mediator';
 
-    var originalPageTitle = document.title;
 
-    function NotificationCounter() {
+var originalPageTitle = document.title;
 
+function NotificationCounter() {
+
+}
+
+NotificationCounter.prototype.init = function() {
+    var self = this;
+    mediator.on('modules:autoupdate:unread', function(count) {
+        self.setCount(count);
+    });
+};
+
+NotificationCounter.prototype.setCount = function(count) {
+    if (count > 0) {
+        document.title = '(' + count + ') ' + originalPageTitle;
+    } else {
+        this.restorePageTitle();
     }
+};
 
-    NotificationCounter.prototype.init = function () {
-        var self = this;
-        mediator.on('modules:autoupdate:unread', function (count) {
-            self.setCount(count);
-        });
-    };
+NotificationCounter.prototype.restorePageTitle = function() {
+    document.title = originalPageTitle;
+};
 
-    NotificationCounter.prototype.setCount = function (count) {
-        if (count > 0) {
-            document.title = '(' + count + ') ' + originalPageTitle;
-        } else {
-            this.restorePageTitle();
-        }
-    };
-
-    NotificationCounter.prototype.restorePageTitle = function () {
-        document.title = originalPageTitle;
-    };
-
-    return NotificationCounter;
-});
+export default NotificationCounter;
