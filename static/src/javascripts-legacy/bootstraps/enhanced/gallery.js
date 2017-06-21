9b7b9f542dc94bfb89924e4d379a44b2c9a552b4
Alex Sanders <a@snd.rs>
2017-06-21 11:12:13 +0100

    convert module

--- static/src/javascripts-legacy/bootstraps/enhanced/gallery.js
+++ static/src/javascripts-legacy/bootstraps/enhanced/gallery.js
@@ -1,40 +1,30 @@
-define([
-    'qwery',
-    'lib/config',
-    'lib/mediator',
-    'common/modules/component',
-    'bootstraps/enhanced/trail'
-], function (
-    qwery,
-    config,
-    mediator,
-    Component,
-    trail
-) {
-    var transcludeMostPopular = function () {
-            var mostViewed = new Component(),
-                container = qwery('.js-gallery-most-popular')[0];
+import qwery from 'qwery';
+import config from 'lib/config';
+import mediator from 'lib/mediator';
+import Component from 'common/modules/component';
+import trail from 'bootstraps/enhanced/trail';
+var transcludeMostPopular = function() {
+        var mostViewed = new Component(),
+            container = qwery('.js-gallery-most-popular')[0];
 
-            mostViewed.manipulationType = 'html';
-            mostViewed.endpoint = '/gallery/most-viewed.json';
-            mostViewed.ready = function () {
-                mediator.emit('page:new-content', container);
-            };
-            mostViewed.fetch(container, 'html');
-        },
-        ready = function () {
-            trail();
-
-            mediator.emit('ui:images:upgradePictures');
-
-            mediator.emit('page:gallery:ready');
-            if (config.page.showRelatedContent) {
-                transcludeMostPopular();
-            }
+        mostViewed.manipulationType = 'html';
+        mostViewed.endpoint = '/gallery/most-viewed.json';
+        mostViewed.ready = function() {
+            mediator.emit('page:new-content', container);
         };
+        mostViewed.fetch(container, 'html');
+    },
+    ready = function() {
+        trail();
 
-    return {
-        init: ready
+        mediator.emit('ui:images:upgradePictures');
+
+        mediator.emit('page:gallery:ready');
+        if (config.page.showRelatedContent) {
+            transcludeMostPopular();
+        }
     };
 
-});
+export default {
+    init: ready
+};
