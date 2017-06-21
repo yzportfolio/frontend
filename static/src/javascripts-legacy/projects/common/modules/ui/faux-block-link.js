724a5967b6b94b509de6977799d6332b0fd32d09
Alex Sanders <a@snd.rs>
2017-06-21 11:21:55 +0100

    convert static/src/javascripts-legacy/projects/common/modules/ui/faux-block-link.js

--- static/src/javascripts-legacy/projects/common/modules/ui/faux-block-link.js
+++ static/src/javascripts-legacy/projects/common/modules/ui/faux-block-link.js
@@ -1,32 +1,24 @@
-define([
-    'bean',
-    'bonzo',
-    'fastdom',
-    'lib/$'
-], function (
-    bean,
-    bonzo,
-    fastdom,
-    $
-) {
-    var overlaySelector = '.u-faux-block-link__overlay',
-        hoverStateClassName = 'u-faux-block-link--hover';
+import bean from 'bean';
+import fastdom from 'fastdom';
+import $ from 'lib/$';
 
-    return function () {
-        var showIntentToClick = function (e) {
-            fastdom.write(function () {
-                $(e.currentTarget).parent().addClass(hoverStateClassName);
-            });
-        };
-        var removeIntentToClick = function (e) {
-            fastdom.write(function () {
-                $(e.currentTarget).parent().removeClass(hoverStateClassName);
-            });
-        };
+var overlaySelector = '.u-faux-block-link__overlay',
+    hoverStateClassName = 'u-faux-block-link--hover';
 
-        // mouseover
-        bean.on(document.body, 'mouseenter', overlaySelector, showIntentToClick);
-        // mouseout
-        bean.on(document.body, 'mouseleave', overlaySelector, removeIntentToClick);
+export default function() {
+    var showIntentToClick = function(e) {
+        fastdom.write(function() {
+            $(e.currentTarget).parent().addClass(hoverStateClassName);
+        });
     };
-});
+    var removeIntentToClick = function(e) {
+        fastdom.write(function() {
+            $(e.currentTarget).parent().removeClass(hoverStateClassName);
+        });
+    };
+
+    // mouseover
+    bean.on(document.body, 'mouseenter', overlaySelector, showIntentToClick);
+    // mouseout
+    bean.on(document.body, 'mouseleave', overlaySelector, removeIntentToClick);
+}
